c0104cf7596e372df07ca3b14f199b0772f7a5d3
Peter Jang <peter.x.jang@gmail.com>
2018-03-01 20:23:41 -0600

    Add basic solution to dictionary app

---
+++ dictionary_app.rb
@@ -0,0 +1,9 @@
+require "unirest"
+
+system "clear"
+puts "Welcome to the Dictionary App!"
+print "Enter a word: "
+input_word = gets.chomp
+
+response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
+p response.body[0]["text"]
